167697cd58b1da59246c2dda6f46eb0b6e104232
Quentin Lhoest <lhoest.q@gmail.com>
2020-06-04 23:55:40 +0200

    add doc and queries embedder in IndexedDataset

--- src/nlp/indexed_dataset.py
+++ src/nlp/indexed_dataset.py
@@ -2,7 +2,7 @@ import logging
 import math
 import os
 from time import time
-from typing import Dict, List, Tuple, Union
+from typing import Dict, List, Tuple, Union, Callable, Optional
 
 import numpy as np
 
@@ -28,72 +28,73 @@ except ImportError:
 logger = logging.getLogger(__name__)
 
 
+class BaseEmbedModel:
+
+    def embed_documents(self, texts: List[str], titles: Optional[List[str]] = None, tokenizer: Optional[Callable[[str], List[str]]] = None) -> np.array:
+        raise NotImplementedError
+
+    def embed_queries(self, queries: List[str], tokenizer: Optional[Callable[[str], List[str]]] = None) -> np.array:
+        raise NotImplementedError
+
+
 class BaseIndexedDataset:
     def __init__(
         self,
         dataset: Dataset,
-        split_function=None,
-        split_kwargs=None,
-        index_kwargs=None,
-        embed_model=None,
-        embed_tokenizer=None,
+        index_name: str,
+        embed_model: BaseEmbedModel,
+        index_kwargs: Optional[dict] = None,
+        embed_tokenizer: Optional[Callable] = None,
     ):
-        self.dataset = dataset  # an nlp dataset
-        self.dataset_snippets = self.make_snippets(split_function, **split_kwargs)
+        self.dataset = dataset  # an nlp dataset of snippets
+        self.index_name = index_name
         self.embed_model = embed_model  # a model that needs to have an embed_query and embed_documents method
         # e.g. https://github.com/yjernite/transformers/blob/58e06cda0bf004ae84ab2381b1136814e0907c1d/examples/eli5/eli5_utils.py#L123
         self.embed_tokenizer = embed_tokenizer
+        index_kwargs = index_kwargs if index_kwargs is not None else {}
         self.init_index(**index_kwargs)
 
     def __getitem__(self, key: Union[int, slice, str]) -> Union[Dict, List]:
-        return self.dataset_snippets[key]
+        return self.dataset[key]
 
     def query_index(self, query, k: int = 10) -> Tuple[List[float], List[int]]:
         raise NotImplementedError
 
-    def init_index(self):
+    def init_index(self, **kwargs):
         raise NotImplementedError
 
     def query_index_batch(self, queries, k: int = 10) -> Tuple[List[List[float]], List[List[int]]]:
-        raise NotImplementedError
+        total_scores, total_indices = [], []
+        for query in queries:
+            scores, indices = self.query_index(query, k)
+            total_scores.append(scores)
+            total_indices.append(indices)
+        return total_scores, total_indices
 
     # TODO: batch version of query_dense_index
     #   https://github.com/yjernite/transformers/blob/58e06cda0bf004ae84ab2381b1136814e0907c1d/examples/eli5/eli5_utils.py#L530
-
-    # we need to split the dataset items (e.g. Wikipedia articles) into fixed sized
-    # passages / snippets to index
-    def make_snippets(self, split_func, **split_args) -> Dataset:
-        return self.dataset
-        # TODO: implement
-        # My first implementation makes an NLP dataset of snippets:
-        #   https://github.com/yjernite/transformers/blob/eli5_examples/examples/eli5/data/wiki_snippets/wiki_snippets.py
-        # here are the split functions for Wikipedia and Wiki40b:
-        #   https://github.com/yjernite/transformers/blob/58e06cda0bf004ae84ab2381b1136814e0907c1d/examples/eli5/data/wiki_snippets/wiki_snippets.py#L89
-        #   https://github.com/yjernite/transformers/blob/58e06cda0bf004ae84ab2381b1136814e0907c1d/examples/eli5/data/wiki_snippets/wiki_snippets.py#L59
-        # each snippet has the text, article title, titles of sections by the text, and provenance info to map back to self.dataset items
 
 
 class SparseIndexedDataset(BaseIndexedDataset):
 
     # indexes snippets with ElasticSearch
-    def init_index(self, es_client: es.Elasticsearch, index_name):
+    def init_index(self, es_client, **kwargs):
         assert (
             _has_elasticsearch
         ), "You must install ElasticSearch to use SparseIndexedDataset. To do so you can run `pip install elasticsearch`"
         # Elasticsearch needs to be launched in another window, and a python client is declared withL
         # > es_client = Elasticsearch([{'host': 'localhost', 'port': '9200'}])
         self.es_client = es_client
-        self.es_index_name = index_name
         self._make_es_index_snippets()
         # Here's the code for make_es_index_snippets:
         #   https://github.com/yjernite/transformers/blob/58e06cda0bf004ae84ab2381b1136814e0907c1d/examples/eli5/eli5_utils.py#L34
 
-    def make_es_index_snippets(self):
+    def _make_es_index_snippets(self):
         from tqdm.auto import tqdm
 
         es_client = self.es_client
-        passages_dset = self.dataset_snippets
-        index_name = self.dataset.info.builder_name  # TODO: use full name
+        passages_dset = self.dataset
+        index_name = self.index_name
         index_config = {
             "settings": {
                 "number_of_shards": 1,
@@ -125,7 +126,7 @@ class SparseIndexedDataset(BaseIndexedDataset):
     # send query to the elastic_search client, and return list of snippets
     def query_index(self, query, k=10):
         response = self.es_client.search(
-            index=self.es_index_name,
+            index=self.index_name,
             body={
                 "query": {
                     "multi_match": {
@@ -145,63 +146,75 @@ class DenseIndexedDataset(BaseIndexedDataset):
     def __init__(
         self,
         dataset: Dataset,
-        split_function=None,
-        split_kwargs=None,
-        index_kwargs=None,
-        embed_model=None,
-        embed_tokenizer=None,
-        batch_size=10_000,
-        size=128,
+        index_name: str,
+        embed_model: BaseEmbedModel,
+        index_kwargs: Optional[dict] = None,
+        embed_tokenizer: Optional[Callable] = None,
+        batch_size=16,
+        size=768,
     ):
-        self.array_file_path = os.path.join(HF_INDEXES_CACHE, dataset.info.builder_name)
-        super().__init__(dataset, split_function, split_kwargs, index_kwargs, embed_model, embed_tokenizer)
+        self.array_file_path = os.path.join(HF_INDEXES_CACHE, index_name)
         self.batch_size = batch_size
         self.size = size
+        super().__init__(dataset, index_name, embed_model, index_kwargs, embed_tokenizer)
 
     # batch computes the snippet text representation with the model and add to memory-mapped numpy file
     # Let's start with Numpy, then investigate how pyarrow Tensors would work
-    def init_index(self, device=-1):
+    def init_index(self, device=-1, **kwargs):
         assert (
             _has_faiss
         ), "You must install Faiss to use DenseIndexedDataset. To do so you can run `pip install faiss`"
+        os.makedirs(HF_INDEXES_CACHE, exist_ok=True)
         if not os.path.exists(self.array_file_path):
+            logger.info("Building dense index with name '{}'.".format(self.index_name))
             self._build_dense_index()
+        else:
+            logger.info("Dense index with name '{}' already exist, loading it.".format(self.index_name))
         self._load_dense_index(device)
+        logger.info("Dense index '{}' loaded.".format(self.index_name))
 
     def _build_dense_index(self):
         st_time = time()
-        logger.info("Building dense index for dataset {}.".format(self.dataset.info.builder_name))
         fp = np.memmap(
-            self.array_file_path, dtype="float32", mode="w+", shape=(self.dataset_snippets.num_rows, self.size)
+            self.array_file_path, dtype="float32", mode="w+", shape=(self.dataset.num_rows, self.size)
         )
-        n_batches = math.ceil(self.dataset_snippets.num_rows / self.batch_size)
+        n_batches = math.ceil(self.dataset.num_rows / self.batch_size)
         for i in range(n_batches):
-            passages = [
-                p for p in self.dataset_snippets[i * self.batch_size : (i + 1) * self.batch_size]["passage_text"]
+            texts = [
+                p for p in self.dataset[i * self.batch_size : (i + 1) * self.batch_size]["text"]
             ]
-            reps = self.embed_model.embed_documents(passages, self.embed_tokenizer)
+            titles = [
+                p for p in self.dataset[i * self.batch_size : (i + 1) * self.batch_size]["title"]
+            ]
+            reps = self.embed_model.embed_documents(texts, titles=titles, tokenizer=self.embed_tokenizer)
             fp[i * self.batch_size : (i + 1) * self.batch_size] = reps
             if i % 50 == 0:
-                logger.info(i, time() - st_time)
-        fp.close()
+                logger.info("Done writing batch={}/{},\ttime={:.2f}s".format(i + 1, n_batches, time() - st_time))
+        fp.flush()
+        del fp
 
     # load the numpy index into faiss
     # device is the index of the GPU, -1 for CPU
     def _load_dense_index(self, device=-1):
         fp = np.memmap(
-            self.array_file_path, dtype="float32", mode="r", shape=(self.dataset_snippets.num_rows, self.size)
+            self.array_file_path, dtype="float32", mode="r", shape=(self.dataset.num_rows, self.size)
         )
-        faiss_res = faiss.StandardGpuResources()
         index_flat = faiss.IndexFlatIP(self.size)
         if device > -1:
+            faiss_res = faiss.StandardGpuResources()
             self.faiss_index = faiss.index_cpu_to_gpu(faiss_res, device, index_flat)
         else:
             self.faiss_index = index_flat
         self.faiss_index.add(fp)
-        fp.close()
+        fp.flush()
 
     # query the dense index either qith a text query or a pre-computed query vector
     def query_index(self, query, k=10):
-        q_rep = self.embed_model.embed_query(query, self.embed_tokenizer)
-        scores, indices = self.fais_index.search(q_rep, self.k)
+        q_rep = self.embed_model.embed_queries([query], self.embed_tokenizer)
+        scores, indices = self.faiss_index.search(q_rep, k)
         return scores[0], indices[0]
+
+    def query_index_batch(self, queries, k=10):
+        q_rep = self.embed_model.embed_queries(queries, self.embed_tokenizer)
+        scores, indices = self.faiss_index.search(q_rep, k)
+        return scores, indices
